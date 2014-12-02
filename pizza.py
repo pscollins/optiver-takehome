dbaa8e95f1becb7310236fc3c81cfb2481e637ac
pscollins <pscollins@uchicago.edu>
2014-12-02 04:59:14 -0600

    big rewrite

--- pizza.py
+++ pizza.py
@@ -3,104 +3,68 @@ import abc
 
 from sys import argv
 from operator import attrgetter
+from collections import namedtuple
 
-class ComparableMixin:
-    def __eq__(self, obj):
-        return isinstance(obj, self.__class__) and self.__dict__ == obj.__dict__
+BAKE_TIMES = {
+    '0': 60,
+    '1': 15
+}
 
+OVEN_COUNT = 3
 
-class Clock(ComparableMixin):
-    '''A wraper around a mutable integer to keep track of the current time.'''
-
+class Clock:
     def __init__(self):
         self.time = 0
 
-    def advance(self, pizza):
-        '''Advance the time by the correct amount to complete baking a pizza
-and return the new current time.'''
-        # Make sure we don't bake pizzas from the future
-        # self.time = max(self.time, pizza.placed_at)
-        # self.time += pizza.bake_time
-        self.time = pizza.finished_at
-        return self.time
-
-class Ovens(ComparableMixin):
-    OVEN_COUNT = 3
-
+class Oven:
     def __init__(self, clock):
-        self._orders = []
-        self._results = []
-        self._clock = clock
+        self.cooking = None
+        self.finishing_at = 0
+        self.clock = clock
 
-    def _pop_next(self):
-        # This is O(n), but we can't do any better with an array
-        # to_bake = min(self._orders, key=attrgetter("bake_time"))
-        to_bake = min(self._orders, key=attrgetter("finished_at"))
-        self._clock.advance(to_bake)
-        self._results.append(to_bake.bake(self._clock))
-        self._orders.remove(to_bake)
+    def retrieve(self):
+        assert(self.cooking is not None)
+
+        self.clock.time = self.finishing_at
+
+        finished = BakedPizza(self.cooking.num,
+                              self.cooking.placed_at,
+                              self.finishing_at - self.cooking.placed_at)
+        self.cooking = None
 
     def submit(self, pizza):
-        if len(self._orders) > self.OVEN_COUNT:
-            self._pop_next()
+        self.cooking = pizza
+        self.finishing_at = self.clock.time + pizza.bake_time
 
-        pizza.bake(self._clock)
-        self._orders.append(pizza)
+    def bake(self, pizza):
+        finished = self.retrieve()
+        self.submit(pizza)
+        return finished
 
+class Ovens:
+    def __init__(self, ovens):
+        self.ovens = ovens
+        self.baked_pizzas = []
 
+    def next_available(self):
+        return min(self.ovens, key=attrgetter("finishing_at"))
 
-    def flush(self):
-        while self._orders:
-            self._pop_next()
+    def bake(self, pizza):
+        finished = self.next_available().bake(pizza)
+        if finished is not None:
+            self.baked_pizzas.append(finished)
 
-        return self._results
-
-class AbstractPizza(ComparableMixin, metaclass=abc.ABCMeta):
-    '''The abstract base class for our two concrete pizza types.'''
-
-    def __init__(self, placed_at, num):
-        self.placed_at = int(placed_at)
-        self.num = int(num)
-        self.finished_at = None
-
-    def bake(self, clock):
-        '''Increment the system clock, set the finished time of the pizza, and
-return a the result to be written out to the result file.'''
-
-        # Only bake once
-        if self.finished_at is None:
-            self.finished_at = max(clock.time, self.placed_at) + self.bake_time
-
-        return self._result()
-
-    def _result(self):
-        return (self.num, self.finished_at, self.finished_at - self.placed_at)
-
-
-class DeepDishPizza(AbstractPizza):
-    def __init__(self, placed_at, num):
-        super().__init__(placed_at, num)
-        self.bake_time = 60
-
-class ThinCrustPizza(AbstractPizza):
-    def __init__(self, placed_at, num):
-        super().__init__(placed_at, num)
-        self.bake_time = 15
+UnbakedPizza = namedtuple("Pizza", ["placed_at", "num", "bake_time"])
+BakedPizza = namedtuple("BakedPizza", ["num", "placed_at", "finished_at"])
 
 def parse(infile):
     '''Given a file handle corresponding to the input CSV, construct a
 list of Pizza objects.'''
 
-    clock = Clock()
     reader = csv.reader(infile)
 
-    cons = {
-        '0': lambda placed_at, num: DeepDishPizza(placed_at, num),
-        '1': lambda placed_at, num: ThinCrustPizza(placed_at, num)
-    }
-
-
-    pizzas = [cons[ty](placed_at, num) for placed_at, num, ty in reader]
+    pizzas = [Pizza(placed_at, num, BAKE_TIMES[ty])
+              for placed_at, num, ty in reader]
 
     return pizzas
 
@@ -109,10 +73,11 @@ def write(pizzas, outfile):
 write them out to the result file.'''
 
     writer = csv.writer(outfile)
-    ovens = Ovens(Clock())
+    clock = Clock()
+    ovens = Ovens(Oven(clock) for _ in range(OVEN_COUNT))
 
     for pizza in pizzas:
-        ovens.submit(pizza)
+        ovens.bake(pizza)
 
     writer.writerows(ovens.flush())
 
